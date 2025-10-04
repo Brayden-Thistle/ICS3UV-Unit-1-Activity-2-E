d53d2d666e53bfe40d5427c930dbccdf5050a015
default <bthistle@huronperthcatholic.ca>
2025-10-04 04:43:06 +0000

    added a program that displays my friends dogs weight in lbs

---
+++ main.go
@@ -0,0 +1,18 @@
+/*
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-10-04
+ * @fileoverfiew This program displays my friends dogs weight in kg and lbs
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	
+	fmt.Println("If my dog weighs 34.5 kg, what is that in pounds?")
+	fmt.Println("34.5 kg = " + fmt.Sprint(34.5 * 2.20462) + "pounds")
+
+	fmt.Println("\nDone")
+}
---
+++ main.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-10-04
+ * @fileoverfiew this program tells me my friends dogs weight from kg to lbs
+ */
+
+console.log("My friend's dog weighs 34.5kg")
+console.log("34.5kg is equal to " + (34.5 * 2.20462 ) + "lbs");
+console.log("My friend's dog weights 34.5kg or 76.059391lbs");
+
+console.log("\nDone");
